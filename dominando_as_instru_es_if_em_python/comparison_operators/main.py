a84eafceb786fe145deff9528dd7d08f4b5ace0c
lucasfernandescoder <>
2026-07-06 16:20:04 +0000

    Comparison Operators

---
+++ dominando_as_instru_es_if_em_python/comparison_operators/main.py
@@ -0,0 +1,14 @@
+street_temperature = 15
+hydration_goal_met = True
+
+running_temp = False
+
+___
+	running_temp = ___
+
+# Testing
+print("Are the conditions ideal for running now?", running_temp)
+
+# Hydration goal check
+___
+    print("Great job meeting your hydration goal!")
